6932aead9cb328f94bb32d8f5e5cf1b5031a66db
SverkerBerggren <82815654+SverkerBerggren@users.noreply.github.com>
2022-10-13 12:52:38 +0200

    Fixade ip

--- Assets/Scripts/Multiplayer (LAN)/IpAdressInputField.cs
+++ Assets/Scripts/Multiplayer (LAN)/IpAdressInputField.cs
@@ -46,7 +46,7 @@ public class IpAdressInputField : MonoBehaviour
 
     public void ConnectToServer()
     {
-        clientConnection.ConnectToServer("193.10.9.89", 62000);
+        clientConnection.ConnectToServer("193.10.9.96", 62000);
 
 
 
