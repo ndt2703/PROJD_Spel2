aab356dbd3096c74a82fdc92159c43e484dd2e43
fionaliu66 <fiona.liu0606@gmail.com>
2022-10-19 21:45:51 +0200

    little fix

--- Assets/Scripts/VFX/Card_Animation.cs
+++ Assets/Scripts/VFX/Card_Animation.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Card_Animation : MonoBehaviour
 {
     // Start is called before the first frame update
+   
+   
+
     private void OnMouseEnter()
     {
+     
         transform.localScale = new Vector3(1.4f, 1.4f, 1.4f);
+  
     }
     private void OnMouseExit()
     {
+     
         transform.localScale = Vector3.one;
+
     }
 }
