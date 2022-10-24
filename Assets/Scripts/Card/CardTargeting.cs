fb7cede82db7254bcbbb959fe16288823994cd9d
MarcusHedenberg <mackanhed@gmail.com>
2022-10-24 12:53:51 +0200

    Backface cards

--- Assets/Scripts/Card/CardTargeting.cs
+++ Assets/Scripts/Card/CardTargeting.cs
@@ -31,7 +31,7 @@ public class CardTargeting : MonoBehaviour
 
     private void OnMouseUp()
     {
-        mousePosition = GetComponent<CardMovement>().mousePosition;
+        mousePosition = cardMovement.mousePosition;
         card = cardDisplay.card;
 
         RaycastHit hitEnemy;
@@ -68,7 +68,7 @@ public class CardTargeting : MonoBehaviour
         {
             case "Champion":
                 card.Target = gameObjectHit.GetComponent<Champion>();
-                if (actionofPlayer.CheckIfCanPlayCard(card, true))
+                if (actionOfPlayer.CheckIfCanPlayCard(card, true))
                 {                    
                     card.PlayCard();
                     cardDisplay.card = null;
