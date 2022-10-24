09de4973d59e00abaf4d69dfee7d675a54c6867a
SverkerBerggren <82815654+SverkerBerggren@users.noreply.github.com>
2022-10-24 16:06:47 +0200

    Update CardTargeting.cs

--- Assets/Scripts/Card/CardTargeting.cs
+++ Assets/Scripts/Card/CardTargeting.cs
@@ -67,7 +67,7 @@ public class CardTargeting : MonoBehaviour
         switch (gameObjectHit.tag)
         {
             case "Champion":
-                card.Target = gameObjectHit.GetComponent<Champion>();
+                card.Target = gameObjectHit.GetComponent<AvailableChampion>().champion;
                 if (actionOfPlayer.CheckIfCanPlayCard(card, true))
                 {                    
                     card.PlayCard();
