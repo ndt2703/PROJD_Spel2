23cbb76397b7eb887ecebe4cea84fb8b4b64b281
SverkerBerggren <82815654+SverkerBerggren@users.noreply.github.com>
2022-10-25 10:39:49 +0200

    Update TargetAndAmount.cs

--- Assets/Scripts/TargetAndAmount.cs
+++ Assets/Scripts/TargetAndAmount.cs
@@ -1,18 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class TargetAndAmount : MonoBehaviour
+public class TargetAndAmount
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public TargetInfo targetInfo;
+    public int amount;
 }
