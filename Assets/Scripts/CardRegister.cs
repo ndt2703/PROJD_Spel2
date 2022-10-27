a8cd9c345253dbc2d8f0a01b2f0b0b37950642e2
SverkerBerggren <82815654+SverkerBerggren@users.noreply.github.com>
2022-10-27 15:58:56 +0200

    yes

--- Assets/Scripts/CardRegister.cs
+++ Assets/Scripts/CardRegister.cs
@@ -9,29 +9,33 @@ public class CardRegister : MonoBehaviour
 
     [SerializeField] private List<Card> cards = new List<Card>();
     public Dictionary<string, Card> cardRegister = new Dictionary<string, Card>();
+
+
+    [SerializeField] private List<Champion> champions = new List<Champion>();
+    public Dictionary<string, Champion> champRegister = new Dictionary<string, Champion>();
     // Start is called before the first frame update
 
     private void Awake()
     {
         if (Instance == null)
         {
-            
             Instance = this;
         }
         else
         {
             Destroy(Instance);
         }
+
         foreach (Card card in cards)
         {
             cardRegister.Add(card.cardName, card);
         }
+
+        foreach (Champion champion in champions)
+        {
+            champRegister.Add(champion.name, champion);
+        }
+
         DontDestroyOnLoad(this);
-    }
-
-
-    void Start()
-    {
-
     }
 }
